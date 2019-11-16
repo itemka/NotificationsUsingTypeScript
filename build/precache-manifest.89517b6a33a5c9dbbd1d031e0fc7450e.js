c13b4cfb0b663aac1aa69e46ec2d05436a394e95
itemka <itemka2503@gmail.com>
2019-11-17 00:39:55 +0300

    I added this project to gh-pages

---
+++ build/precache-manifest.89517b6a33a5c9dbbd1d031e0fc7450e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42519c81462bef6edbac888717e66be0",
+    "url": "/NotificationsUsingTypeScript/index.html"
+  },
+  {
+    "revision": "fefa889f19f06c16ecd5",
+    "url": "/NotificationsUsingTypeScript/static/css/main.0c930483.chunk.css"
+  },
+  {
+    "revision": "cea451bedbdd555c0e01",
+    "url": "/NotificationsUsingTypeScript/static/js/2.3b3d1d4c.chunk.js"
+  },
+  {
+    "revision": "fefa889f19f06c16ecd5",
+    "url": "/NotificationsUsingTypeScript/static/js/main.ad915b41.chunk.js"
+  },
+  {
+    "revision": "202adafab2ba93f7b9ab",
+    "url": "/NotificationsUsingTypeScript/static/js/runtime-main.0f58611e.js"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,15 @@ interface IObjectOfProps {
 
 class App extends Component<IObjectOfProps> {
 
-    onClickButtonSuccess = () => {
-        this.props.AddNotificationThunk(`Success`, `someText`, `Success`);
-    };
-    onClickButtonWarning = () => {
-        this.props.AddNotificationThunk(`Warning`, `someText`, `Warning`);
-    };
-    onClickButtonError = () => {
-        this.props.AddNotificationThunk(`Error`, `someText`, `Error`);
-    };
+    onClickButtonSuccess = () => this.props.AddNotificationThunk(`Success`, `someText`, `Success`);
+    onClickButtonWarning = () => this.props.AddNotificationThunk(`Warning`, `someText`, `Warning`);
+    onClickButtonError = () => this.props.AddNotificationThunk(`Error`, `someText`, `Error`);
 
     render() {
         let notification = this.props.notificationArray.map(item => <Message key={item.id} style={`${item.types}`}
                                                                              title={`${item.title}`}
                                                                              text={`${item.text}`}/>);
         return (
-
             <div className="App">
                 <Button style={`buttonSuccess button`} callback={this.onClickButtonSuccess} nameButton={'Success'}/>
                 <Button style={`buttonWarning button`} callback={this.onClickButtonWarning} nameButton={'Warning'}/>
@@ -38,7 +31,6 @@ class App extends Component<IObjectOfProps> {
                     {notification}
                 </div>
             </div>
-
         );
     }
 }
